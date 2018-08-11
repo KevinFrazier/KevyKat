937362f106a58656cb4995392fb8f515ea39da3d
unknown <kfraz001@ucr.edu>
2018-08-11 16:27:24 -0700

    added comments to MainActivity

--- KevyKat/app/src/main/java/com/kevykat/kevykat/MainActivity.java
+++ KevyKat/app/src/main/java/com/kevykat/kevykat/MainActivity.java
@@ -8,9 +8,20 @@ public class MainActivity extends AppCompatActivity {
     GameView g;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //calling parent function with "super"
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+
+        //initalizing new GameView object
         g = new GameView(this);
+
+        //sets object as current phone screen
+
+        //screen for the game
         setContentView(g);
+
+        //screen for activity.main.xml layout
+        //setContentView(R.layout.activity_main);
+
     }
 }
