6e4916a0875e4cde8d0fbb6e44c7dd4e8246e9c4
unknown <kfraz001@ucr.edu>
2018-08-11 15:42:44 -0700

    added GameView

--- KevyKat/app/src/main/java/com/kevykat/kevykat/MainActivity.java
+++ KevyKat/app/src/main/java/com/kevykat/kevykat/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    GameView g;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        g = new GameView(this);
+        setContentView(g);
     }
 }
